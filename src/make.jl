7e78da3d9436f086a1ac6bb481c01b62b7391dcf
Baptiste Combelles <baptiste.combelles@proton.me>
2023-07-07 11:16:05 +0200

    change path assets

--- src/make.jl
+++ src/make.jl
@@ -59,7 +59,7 @@ MultiDocumenter.make(
     outpath,
     docs;
     rootpath = "/CTDocs.jl/",
-    brand_image = MultiDocumenter.BrandImage("https://control-toolbox.org/",joinpath("assets","ct-crop.svg")),
+    brand_image = MultiDocumenter.BrandImage("https://control-toolbox.org/",joinpath("src/assets","ct-crop.svg")),
 )
 
 gitroot = normpath(joinpath(@__DIR__, ".."))
